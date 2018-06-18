0d488db70d853410ade565c19468ea3c6a83b1e0
touchy0912 <touchy0912@gmail.com>
2018-06-18 06:21:51 +0000

    user login

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class='text-center'>
+    <h1>Log in</h1>
+</div>
+
+<div class="row">
+<div class='col-mid-6 col-mid-offset-3'>
+    {!! Form::open(['route'=>'login.post']) !!}
+    <div class='form-group'>
+        {!! Form::label('email','Email') !!}
+        {!! Form::email('email',old('email'),['class'=>'form-control'])  !!}
+    </div>
+        <div class='form-group'>
+        {!! Form::label('password','Password') !!}
+        {!! Form::password('password',['class'=>'form-control'])  !!}
+    </div>
+    
+    {!! Form::submit('Log in',['class'=>'btn btn-primary btn-block']) !!}
+    
+    {!!  Form::close() !!}
+    
+    <p>New User?{!! link_to_route('signup.get','Sign up Now!') !!}</p>
+    
+</div>
+</div>
+
+
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
+@if(Auth::check())
+<?php $user=Auth::user();  ?>
+{{$user->name}}
+
+@else
     <div class='center jumbotron'>
         <div class='text-center'>
             <h1>Welcome to the Microposts</h1>
@@ -9,7 +14,7 @@
         
         
     </div>
-
+@endif
 
 
 
